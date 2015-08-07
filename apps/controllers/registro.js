79cde023b510a9aa958c6b9a065dccaefdd275ca
Juan Carlos Medina <jcmedina@newchannel.mx>
2015-08-07 18:02:57 -0500

    vista de registro de médicos para directorio
    En home se añadió un botón que redirecciona a la pagina de registro de
    médicos para introducirlos al directorio.

---
+++ apps/controllers/registro.js
@@ -0,0 +1,17 @@
+/**
+* 	En este archivo se atraparan los eventos de la plantilla home.handlebars, 
+*	asi como también hara la función de recibir y enviar los datos del modelo,
+*	hacía la vista. Se trabajara con una función que simulara como la clase de este
+*	archivo y dentro de esta función es como se trabajara.
+*	Dentro de los metodos iran las llamadas a los metodos o llamados de los modelos y el envio de los parametros 
+*	@author Oscar, Cinthia.
+*	@version 0.0.0.0
+*/
+//librerias que se utilizaran en este archivo
+module.exports = {
+	index: function(object, req, res) {
+		//console.log("Esto es un arreglo "+ object);
+		res.render('registro', {}, function(err, html){ res.send(html)});
+		//if(next) next();	
+	}
+}
